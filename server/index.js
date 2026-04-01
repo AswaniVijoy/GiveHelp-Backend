3cf6d361342ad59a54cb87774119546541bca53c
AswaniVijoy <aswanivijoy2002@gmail.com>
2026-04-01 17:09:26 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -25,8 +25,13 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB Error:", err));
 
+app.get("/", (req, res) => {
+  res.json({ message: "GiveHelp API is running!" });
+});
+
 app.use("/user", user);
 app.use("/campaign", campaign);
 app.use("/admin", admin);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
